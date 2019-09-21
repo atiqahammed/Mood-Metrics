766e97e80c48d146faea9f692bb3dd671ed41115
Atiq Ahammed <30492609+atiqahammed@users.noreply.github.com>
2019-09-22 00:52:39 +0600

    project is readable

--- index.js
+++ index.js
@@ -52,8 +52,52 @@ function processFile(inputFile) {
     });
 }
 
-walk('./ok/', function(filePath, stat) {
+
+let project_name = './Angular-JumpStart/'; 
+
+walk(project_name, function(filePath, stat) {
     // do something with "filePath"...
     console.log(filePath)
-    processFile(filePath)
+    // processFile(filePath)
 });
+
+// const fs = require('fs');
+
+// function walk(currentDirPath, callback) {
+//     var fs = require('fs'),
+//         path = require('path');
+//     fs.readdir(currentDirPath, function (err, files) {
+//         if (err) {
+//             throw new Error(err);
+//         }
+//         files.forEach(function (name) {
+//             var filePath = path.join(currentDirPath, name);
+//             var stat = fs.statSync(filePath);
+//             if (stat.isFile()) {
+//                 callback(filePath, stat);
+//             } else if (stat.isDirectory()) {
+//                 walk(filePath, callback);
+//             }
+//         });
+//     });
+// }
+
+// function getFilesPath(projectPath) {
+
+//     let filePaths = new Array();
+//     // console.log()
+
+//     walk('../'+projectPath, function(filePath, stat) {
+//         filePaths.push(filePath);
+//     });
+
+//     return filePaths;
+// }
+
+// // var projectFileReader = require('./src/projectReader')
+
+// let arr = getFilesPath('./Angular-JumpStart/')
+
+// console.log(arr.length)
+
+// console.log('hello')
